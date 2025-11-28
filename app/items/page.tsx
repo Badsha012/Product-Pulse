51399920421d09e2978e52311ee3c32b770b3284
Badsha012 <badshasheikh361@gmail.com>
2025-11-28 16:50:45 +0600

    added a reademe file

--- app/items/page.tsx
+++ app/items/page.tsx
@@ -23,7 +23,7 @@ export default async function ProductsPage() {
 
   return (
     <div className="max-w-6xl mx-auto p-6">
-      <h1 className="text-3xl font-bold mb-6 text-pink-600">All Products</h1>
+      <h1 className="text-3xl font-bold mb-6  text-pink-600">All Products</h1>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         {products.map((product) => (
